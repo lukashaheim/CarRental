bf7044cb9e8279fa8ea6053b8be9353ee26d9487
Lukas Sopp Håheim <lukassh@uia.no>
2025-05-15 13:26:54 +0200

    add cars function added in command line #1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <string>
+#include <SQLiteCpp/SQLiteCpp.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+void addCar(SQLite::Database& db) {
+    string registration, brand, model;
+
+    cout << "Enter registration number: ";
+    getline(cin, registration);
+    cout << "Enter brand: ";
+    getline(cin, brand);
+    cout << "Enter model: ";
+    getline(cin, model);
+
+    SQLite::Statement query(db, "INSERT INTO Cars (registration, brand, model) VALUES (?, ?, ?)");
+    query.bind(1, registration);
+    query.bind(2, brand);
+    query.bind(3, model);
+    query.exec();
+
+    int id = static_cast<int>(db.getLastInsertRowid());
+    cout << "Added car: id: " << id
+         << ", registration: " << registration
+         << ", brand: " << brand
+         << ", model: " << model << "\n";
+}
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    try {
+        SQLite::Database db("../CarRentalNew.sqlite", SQLite::OPEN_READWRITE);
+        addCar(db);
+    } catch (const std::exception& e) {
+        cerr << "Database error: " << e.what() << endl;
+        return 1;
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
